d46a5fe7068d0dafb631a6911aca7e7500be0a2d
windexvalence <windexvalence@proton.me>
2025-01-01 09:07:58 -0600

    add api response secrets and updates

--- build_scripts/gh_action_decrypt_test_html.sh
+++ build_scripts/gh_action_decrypt_test_html.sh
@@ -4,10 +4,23 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-# TODO: change output path to tests/resources/mock_browser_html/<test-filename-here>.html
-for secret_file in secrets/*.gpg; do 
+echo "Decrypting test HTML files ..."
+for secret_file in secrets/mock_browser_html/*.gpg; do 
     out_filename=$(basename $secret_file | sed -nr 's/^(.*)\.gpg$/\1/p')
     out_filepath="tests/resources/mock_browser_html/$out_filename"
+    echo "decrypting input file at '$secret_file' and writing output to '$out_filepath' ..."
+    gpg --quiet \
+        --batch \
+        --yes \
+        --decrypt \
+        --passphrase="$TEST_HTML_PASSPHRASE" \
+        --output "$out_filepath" "$secret_file"
+done
+
+echo "Decrypting test API response JSON files ..."
+for secret_file in secrets/mock_api_responses/*.gpg; do 
+    out_filename=$(basename $secret_file | sed -nr 's/^(.*)\.gpg$/\1/p')
+    out_filepath="tests/resources/mock_api_responses/$out_filename"
     echo "decrypting input file at '$secret_file' and writing output to '$out_filepath' ..."
     gpg --quiet \
         --batch \
